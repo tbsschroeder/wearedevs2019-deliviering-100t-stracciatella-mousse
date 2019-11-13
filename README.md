d81d93ed6e5bd7a38e5c5d67cdc418a504aa0868
Dr. Tobias Schröder <tobias.schroeder@metronom.com>
2019-11-13 16:12:47 +0100

    first commit
    TS: Update Readme
    
    Update README.md
    
    TS: Add QR code
    
    TS: Add Bearlin
    
    TS: Fix logging slide
    
    TS: Update stracciatella slides
    
    TS: Update stracciatella slides
    
    TS: Update stracciatella slides
    
    TS: Add olympic pool fact
    
    TS: Update after rehearsal
    
    TS: Update json example
    
    TS: Update json example
    
    TS: Resize images
    
    TS: Change METRONOM img
    
    TS: Change last slide
    
    TS: Rework after rehearsal
    
    TS: Update gitignore
    
    TS: Rm feedback file
    
    TS: Update badges
    
    TS: Update bug page and mousse
    
    TS: Fix smiley journey
    
    TS: Fix calculations
    
    TS: Add Bjoerns images
    
    Add demo and update index
    
    Update betty images
    
    TS: Add goodharts law
    
    TS: Update demo files
    
    TS: Add pdf func for exporting
    
    TS: Add paper image for the linked paper

---
+++ README.md
@@ -0,0 +1,8 @@
+# How we almost delivered 100 tons of Stracciatella Mousse
+Slides for the WeAreDevelopers Congress 2019 in Vienna
+
+Exporting pdf: Append '?print-pdf' to the url and save the site as pdf
+
+Online version: https://tbsschroeder.github.io/wearedevs2019-deliviering-100t-stracciatella-mousse/#/
+
+Demo project: See demo/
---
+++ demo/sample_data.sql
@@ -0,0 +1,134 @@
+create table random_data(
+	id UUID PRIMARY KEY,
+	confirmed_value INT,
+	invoiced_value INT,
+	time_stamp TIMESTAMP
+);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-29 03:30:47','ABADCB9E-02A4-6CB5-5E21-C494DE74CC71',7500,7500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-29 16:15:14','5A27BF8C-E262-AAE0-CDA7-3FE798630D5D',8000,8000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-29 20:21:20','34ECE52A-2C9F-D9B2-4C0F-F9437BD6FE6F',7700,8500);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-30 17:33:46','91E163E7-B9A6-58F4-189F-A60A6C0BD14C',5500,8000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-30 20:14:08','1BFE226F-EF8E-0CDC-141E-A17EDF5E92A1',7500,7500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-30 21:40:29','D190287C-F5EE-8097-78FE-064AFDAB087A',8000,7500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-30 22:53:32','725B3444-36C9-7F95-E6BA-0DDC2986AB61',9000,10000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-31 01:02:47','3B43E9DB-C161-4E4D-216A-F690CF3F117C',5000,9000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-31 16:38:51','598ECEEB-B723-B860-84C0-14680DA464EB',6000,8000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-10-31 21:47:36','1B19EB17-81F0-16DF-11F5-C992F8C96F75',6000,7000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-01 06:39:13','5F47DD0E-1D3B-C6FE-270C-1918A0F8E481',8000,8000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-01 21:47:03','848FB032-E915-48FD-D362-0C9DCF82E73B',4000,4000);
+
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-02 21:00:00','848FB032-E915-48FD-D362-0C9DCF82E73E',0,0);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-03 21:00:00','848FB032-E915-48FD-D362-0C9DCF82E73F',0,0);
+
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-04 01:08:06','0BC8AAD3-30B4-7B83-5861-C249D062CBC1',4000,4000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-04 05:17:42','C1C5A873-B4EE-A0B6-CBFB-4B6AE0FC95A0',9500,5000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-04 13:33:27','1C728FC2-2575-17BE-D47B-ED34B03B1C19',5000,6000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-05 12:37:39','04252676-3D78-A028-9739-CEA0ED1E42DA',6000,8000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-05 14:57:45','D6C6A703-31C4-49F2-A8EF-66A5B6B38D1D',7000,7500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-05 15:12:42','CF807FB7-0DC3-51AC-DA80-4F335A862C5E',6000,6000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-05 23:37:02','A4C4AF8D-25C7-1366-7707-0996F5951802',7500,7500);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-06 00:57:33','C0C18D51-08C1-3DD7-587C-1B0AA5DC7697',6500,6500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-06 11:31:20','ED164BF5-51BF-DAAF-2106-BB3DB59F99CB',7000,9000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-06 15:37:03','A4224F1D-831A-0CD8-9EFE-469C38B4A0DB',5500,6000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-06 18:46:47','D2EDE95F-DE48-5A5D-31E9-B1EC4E6CFBF2',9000,7000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-06 19:59:18','85C3F721-B32B-5A65-A429-5CB9A967C49E',6500,8500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-06 20:33:21','FDFDC66E-1E7F-C584-50DA-C252D3998D76',8500,8500);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-07 15:27:48','9B985F67-D5B3-BF5E-9978-C41A7FF915F0',6000,7000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-07 17:03:58','D234D72A-2071-55D2-FCC2-D94FA9E6E9BE',7000,9500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-07 21:00:28','483F8DD1-5493-DDE2-EF70-7AE47F086387',9000,9000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-08 04:33:36','C8D49930-2E37-8FC6-5F13-DA7CD01D602D',7700,8400);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-08 10:46:18','BF30D644-9B05-B4C8-ADA9-A7523FD74E3E',7400,9200);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-08 12:47:53','115A2BE2-1978-B33F-9AB8-64C380AD371D',8500,8500);
+
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-09 21:00:00','848FB032-E915-48FD-D362-64C380AD371D',0,0);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-10 21:00:00','848FB032-E915-48FD-D362-A7523FD74E3E',0,0);
+
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-11 05:34:28','C696287E-E126-FF36-607C-DEE347DED318',3000,3400);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-11 13:58:53','2421708D-B8BA-EDC3-E019-1CF68CD87718',3800,4000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-11 14:00:38','83072D5F-4274-D42F-A1CE-11C3AEC4AFE9',4000,4000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-11 19:28:41','428C8C46-38C2-49FF-5A8B-1512325A415D',2000,2000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-12 03:39:13','9D12CE93-1112-9172-25A1-D4C69EA8DBC8',6000,6300);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-12 08:12:30','192739C2-2C8C-270C-D937-4FCCEBC1449C',3800,4000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-12 10:20:57','9A5CAB0B-5616-8A1E-ACC7-5401F0BE69D4',7700,7700);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-12 16:58:39','1ADD4D89-096F-0933-46A7-FDFDFCCCE4C0',5900,6100);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-13 03:36:19','4B59D274-3498-4B3F-FCAA-9669BD693FA9',8000,8000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-13 03:37:16','48201BC5-FD43-E8F9-ABB8-336E9E1A2AB6',5000,5400);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-13 10:09:40','4879F5DE-3E06-FAD1-D6A6-76B134B8273C',8000,8200);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-13 16:49:32','6D18D35D-F2DB-814D-20D3-3D004AE717BE',7500,9000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-14 09:57:31','0CAE37E6-9D4A-5841-F358-F68F4EAEAA94',3400,6000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-14 16:12:01','D8598569-EE0E-2F95-0569-43C93EDE16FE',3300,4000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-15 18:22:16','524E3A12-6D95-779C-ED17-C48BC9B6A4B6',6800,9500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-14 23:30:12','952160DA-C043-FE27-3E48-98769D6162F9',5000,9000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-15 03:02:48','299B97D3-0C5D-07CD-BF89-D2B268031C1D',5500,6100);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-15 10:58:32','7E526355-87A8-8BD2-FDD0-7A1C9CAE076B',4500,5000);
+
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-16 21:00:00','848FB032-E915-48FD-D362-D2B268031C1D',0,0);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-17 21:00:00','848FB032-E915-48FD-D362-7A1C9CAE076B',0,0);
+
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-18 01:12:51','FBC3D90B-644C-2F01-CA7C-124AD8952C21',4000,1450);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-18 05:10:56','183944B4-74F4-380B-961A-51BB9580514E',3200,5500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-18 16:34:33','EF235B18-0098-C67B-10ED-CCD9A9CD7DDF',3400,5400);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-19 03:52:46','71DF30E2-7A71-AF80-0740-BF2443CA012C',5500,5500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-19 06:46:32','634C8A7C-4C6B-A4C8-DBC0-EFCE272379AC',2600,1700);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-19 07:17:53','675A3748-9849-FA09-7D0B-E629E49402E0',5100,9200);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-19 13:01:18','4294FBB0-96F2-6ADC-F3A2-0C14DB83168E',8300,8300);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-20 00:01:17','66A72D5C-E2DC-E92E-DB26-CA314C9AA118',9000,9000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-20 00:07:57','FB2C6607-950D-4636-A57B-9A7751910260',5000,5000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-20 08:36:49','3EFC0522-A754-FEFF-B071-6331003E5D06',7000,7000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-20 15:04:16','FAFF092A-E3EE-C64B-17B4-0D26725A7539',7000,9000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-20 17:14:04','F74FED07-79DF-3DAE-E59A-8DF35E54C7A0',8000,8000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-21 07:40:19','11477D2D-10A6-B8BC-461C-F9D130CFB222',6600,6600);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-21 08:37:52','5ACFD192-2566-78A3-9157-AE1231C3295D',20000,8100);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-21 19:55:22','FC680014-559A-65A6-11BB-B4A1DBEB1749',8000,9000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-22 09:32:11','DC96E196-6661-335E-6367-3B23A1880850',5000,6000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-22 17:20:32','2331B7D5-F4DF-CBE2-E936-ED8B9CC76F0F',5600,5000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-22 23:59:05','E9822A6F-B02E-868E-BA63-ADAEBFF95C2A',3500,400);
+
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-23 21:00:00','848FB032-E915-48FD-D362-F0988EDE1D2C',0,0);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-24 21:00:00','848FB032-E915-48FD-D362-ADAEBFF95C2A',0,0);
+
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-25 03:44:20','82FCC4BA-7B52-DF8B-D63C-3C0A0EFCB775',500,3500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-25 13:41:17','590B79FB-4FC4-98D0-5C4E-4D888D2A83C6',5555,6500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-25 19:25:58','974A4A58-9E03-09E7-9F03-25273414946F',5000,5500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-25 22:50:06','D19BA1BA-86A2-DB58-E868-70AE83DFC8A1',6000,6600);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-26 02:09:47','20D0F41D-010D-C90B-9C7A-A5547FE01D12',7000,7000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-26 09:33:47','A3513AD4-34C2-6B0F-C163-31289ED41475',8700,8700);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-26 09:35:14','BFA5B62E-3B38-4DEB-59DD-7E6021A791DA',6800,6800);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-26 20:05:26','DAD8BAFB-E9CE-EEDB-3FCC-F04B1671B532',7400,7500);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-27 08:37:33','B4C8767C-C544-474F-2988-6BBF56CB73B1',9000,9000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-27 10:47:17','3772ACC7-0909-D76B-69EB-AF99E3F157C1',8500,8500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-27 14:46:55','CA224DBE-3B3A-A94B-7136-5F1C401D9FBD',6700,7600);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-27 22:08:59','018840EB-D9F5-9C47-7F87-ED1CC477532B',7400,7400);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-28 00:56:35','59F71E70-EB36-4209-0559-82E1F6E9C59B',5500,6500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-28 07:39:34','EB4ACE85-ED1B-009F-0AAB-C4835F99A9E7',3500,5000);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-28 13:36:06','F38A9025-AD8B-1AF5-8665-17804DE7FE30',7000,7000);
+
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-29 05:38:35','D08C3B5E-318D-5138-F4AB-E12F65B9EAEE',1000,2500);
+INSERT INTO "random_data" (time_stamp,id,invoiced_value,confirmed_value) VALUES ('2019-11-29 08:19:52','31C1EFD7-7518-2097-DFFE-E47683FA2701',5000,5700);
